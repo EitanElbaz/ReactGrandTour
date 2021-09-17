34211901b2f0d4951d96cdfacfe89a8f646bc1e7
Eitan Elbaz <eitan.elbaz@utilitypoint.co.uk>
2021-09-17 20:39:45 +0100

    added several page examples

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Home, SecondExample } from './Pages';
 
-const App = () => <div>hello</div>;
+const App = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+            <Route path="/second-example">
+                <SecondExample />
+            </Route>
+        </Switch>
+    </BrowserRouter>
+);
 
 export default App;
---
+++ playground/src/Pages/SecondExample.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SecondExample = () => {
+    return <div>SecondExample</div>;
+};
+
+export default SecondExample;
