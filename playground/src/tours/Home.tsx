2ef1e4df6c56a08aefa45a40ebe8cd0e2325efb9
Eitan Elbaz <eitan.elbaz@utilitypoint.co.uk>
2021-09-19 09:22:49 +0100

    fixed lint warning

--- playground/src/tours/Home.tsx
+++ playground/src/tours/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ReactGrandTourStep } from 'react-grand-tour';
-import { Box, Typography } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import { GitHubBtn } from 'components';
 
 export const HomeStepIds = {
