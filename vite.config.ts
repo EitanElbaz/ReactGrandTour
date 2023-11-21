36c31ce84fc41b58997509ec40c08f40578a574f
Eitan Elbaz <EitanElbaz@hollandandbarretts.com>
2023-11-21 11:19:26 +0000

    using react 18 & updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import typescript from '@rollup/plugin-typescript';
 import path from 'path';
 import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [],
+    plugins: [react()],
     resolve: {
         alias: [
             {
@@ -12,9 +13,6 @@ export default defineConfig({
                 replacement: path.resolve(__dirname, './src'),
             },
         ],
-    },
-    esbuild: {
-        minify: true,
     },
     build: {
         manifest: true,
@@ -27,6 +25,11 @@ export default defineConfig({
         },
         rollupOptions: {
             external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                },
+            },
             plugins: [
                 typescriptPaths({
                     preserveExtensions: true,
